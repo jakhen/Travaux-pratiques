766606e9bf5e5393e443a824ada8cecc05b5785d
hchoucounou <jakhen86@gmail.com>
2024-02-08 23:26:52 +0100

    2nd commit test

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -6,21 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    protected Button btn_Validate;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    protected Button mBtn_Validate;
+    protected boolean isBack=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_Validate = findViewById(R.id.Btn_Validate);
+        mBtn_Validate = findViewById(R.id.Btn_Validate);
+        //mBtn_Validate.setEnabled(false);
+        mBtn_Validate.setOnClickListener(this);
 
     }
-    //envoyer
-    public void toActivityQuiz(View view){
-        Intent myIntent=new Intent(this,activityQuiz.class);
-        startActivity(myIntent);
+    protected void onResume(){
+        super.onResume();
+
+        if(isBack){
+            makeToast("Nous sommes heureux de vous revoir");
+            isBack = false;
+        }
+    }
+    protected void onPause() {
+        super.onPause();
+        isBack = true;
+    }
+    Toast t;
+
+    private void makeToast(String s) {
+        if(t != null) t.cancel();
+        t= Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.Btn_Validate){
+            //Toast.makeText(MainActivity.this, "Button Validate clicked2",Toast.LENGTH_SHORT).show();
+            Intent myIntent=new Intent(this,activityQuiz.class);
+            startActivity(myIntent);
+        }
 
     }
+
 }

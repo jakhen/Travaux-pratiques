b78ebab8800bab02db23ab4a60f71cd0de676e63
hchoucounou <jakhen86@gmail.com>
2024-02-08 23:26:52 +0100

    2nd commit test

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -6,21 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    protected Button btn_Validate;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    protected Button mBtn_Validate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_Validate = findViewById(R.id.Btn_Validate);
+        mBtn_Validate = findViewById(R.id.Btn_Validate);
+        //mBtn_Validate.setEnabled(false);
+        mBtn_Validate.setOnClickListener(this);
 
     }
-    //envoyer
-    public void toActivityQuiz(View view){
-        Intent myIntent=new Intent(this,activityQuiz.class);
-        startActivity(myIntent);
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.Btn_Validate){
+            //Toast.makeText(MainActivity.this, "Button Validate clicked2",Toast.LENGTH_SHORT).show();
+            Intent myIntent=new Intent(this,activityQuiz.class);
+            startActivity(myIntent);
+        }
 
     }
+
 }
--- app/src/main/java/com/example/tp1/activityQuiz.java
+++ app/src/main/java/com/example/tp1/activityQuiz.java
@@ -2,13 +2,65 @@ package com.example.tp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class activityQuiz extends AppCompatActivity {
+    ListView listview;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
+    EditText input;
+    ImageView validateNewItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        listview = findViewById(R.id.listview);
+        input = findViewById(R.id.itemInput);
+        validateNewItem = findViewById(R.id.imageAdd);
+        items = new ArrayList<>();
+        items.add("Python");
+        items.add("Java");
+        items.add("Html");
+        items.add("Php");
+        items.add("C++");
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listview.setAdapter(adapter);
+
+        validateNewItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text =input.getText().toString();
+                if(text == null || text.length() == 0){
+                    makeToast("Veuillez saisir l'Element à ajouter");
+                }
+                else{
+                    addItem(text);
+                    input.setText("");
+                    makeToast(text+" a été ajouté");
+
+                }
+            }
+        });
+
+    }
+    public void addItem(String item){
+        items.add(item);
+        listview.setAdapter(adapter);
+    }
+    Toast t;
+    private void makeToast(String s){
+        if(t != null) t.cancel();
+        t= Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
     }
 }
